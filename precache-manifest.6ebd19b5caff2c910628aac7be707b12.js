3b87d486bf8bd35f4acb9c5958cb3df271c34b1a
vladimir3700 <vladdibov23@gmail.com>
2020-10-23 01:38:13 +0300

    Updates

---
+++ precache-manifest.6ebd19b5caff2c910628aac7be707b12.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653ce8eea93d0a573ec13a950883823f",
+    "url": "/Bootcamp_for_developers/index.html"
+  },
+  {
+    "revision": "1a49cc0d98a3a51edb28",
+    "url": "/Bootcamp_for_developers/static/css/2.e0686c58.chunk.css"
+  },
+  {
+    "revision": "4a51ac9e9465449333e6",
+    "url": "/Bootcamp_for_developers/static/css/main.45aea5ee.chunk.css"
+  },
+  {
+    "revision": "1a49cc0d98a3a51edb28",
+    "url": "/Bootcamp_for_developers/static/js/2.a51c33eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bootcamp_for_developers/static/js/2.a51c33eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a51ac9e9465449333e6",
+    "url": "/Bootcamp_for_developers/static/js/main.5312b404.chunk.js"
+  },
+  {
+    "revision": "17ac06005be317bb4654f8392e8f79d3",
+    "url": "/Bootcamp_for_developers/static/js/main.5312b404.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3fdfef8e3af9ed9466",
+    "url": "/Bootcamp_for_developers/static/js/runtime-main.61209dd1.js"
+  },
+  {
+    "revision": "447dd7f35e0098377fcd5a2b60a3c756",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Energetics.447dd7f3.jpg"
+  },
+  {
+    "revision": "d37cb533806248e5b8311737b4d904a5",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Energy.d37cb533.jpg"
+  },
+  {
+    "revision": "bfe2e718dbe66fe7ab639f85b1f42f19",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Industry.bfe2e718.jpg"
+  },
+  {
+    "revision": "4485f9bab83207fc01e6098138ea517c",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Recycling.4485f9ba.jpg"
+  },
+  {
+    "revision": "9349a1f8e2a173a70f2f9668531a5f9c",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Transport.9349a1f8.jpg"
+  },
+  {
+    "revision": "ecbc7a4278787a254033eac61c445a65",
+    "url": "/Bootcamp_for_developers/static/media/Block_policy_Urban_planning.ecbc7a42.jpg"
+  },
+  {
+    "revision": "d3a6a7b0391706f583d0210c3165df2b",
+    "url": "/Bootcamp_for_developers/static/media/calculator.d3a6a7b0.jpg"
+  },
+  {
+    "revision": "f084f5edb225f3a1fc8149a56d2fca6f",
+    "url": "/Bootcamp_for_developers/static/media/flower.f084f5ed.jpg"
+  },
+  {
+    "revision": "0662d9de8af86fb66060a046fbafe1b5",
+    "url": "/Bootcamp_for_developers/static/media/headblock_Global.0662d9de.svg"
+  },
+  {
+    "revision": "2f311a05a79fb86cee6148d658aa8336",
+    "url": "/Bootcamp_for_developers/static/media/headblock_ITN.2f311a05.svg"
+  },
+  {
+    "revision": "341a487c2bb1cbed9179faa73d89b343",
+    "url": "/Bootcamp_for_developers/static/media/headblock_North.341a487c.svg"
+  },
+  {
+    "revision": "f9ea7350b96a78bfb256fd7cc66b9de4",
+    "url": "/Bootcamp_for_developers/static/media/headblock_background_image.f9ea7350.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Bootcamp_for_developers/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "261b9856b2ecca1ae2bb694f9eacab25",
+    "url": "/Bootcamp_for_developers/static/media/m_1.261b9856.jpg"
+  },
+  {
+    "revision": "54f87ec43c16252e0930b4c8720f262f",
+    "url": "/Bootcamp_for_developers/static/media/m_2.54f87ec4.jpg"
+  },
+  {
+    "revision": "5574d6ecf65ccb0a6c22c237a02acaa5",
+    "url": "/Bootcamp_for_developers/static/media/women_children.5574d6ec.jpg"
+  }
+]);
